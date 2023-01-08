937b6ffc235d5b6350405b0328302f383ff33323
Binod Nepali <nepalibinod9@gmail.com>
2023-01-08 19:10:27 +0100

    feat: add vuetify plugin

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,14 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <p>Glad that you had try my template</p>
+
+    <v-btn
+      class="ma-2"
+      variant="text"
+      icon="mdi-thumb-up"
+      color="blue-lighten-2"
+    ></v-btn>
+
+    <v-btn color="primary" class="ma-2"> Vuetify Button </v-btn>
   </div>
 </template>
---
+++ plugins/vuetify.ts
@@ -0,0 +1,26 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+    theme: {
+      themes: {
+        light: {
+          colors: {
+            primary: '#1867C0',
+            secondary: '#5CBBF6',
+          },
+        },
+      },
+    },
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
