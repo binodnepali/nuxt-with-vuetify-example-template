efaec123b23a06596070d3b054b0f023d0f6daac
Binod Nepali <binod.nepali@adidas.com>
2023-12-28 22:09:52 +0100

    feat: update vuetify to latest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
   },
 })
